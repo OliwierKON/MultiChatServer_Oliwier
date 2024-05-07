082e8bdc020e0eb6f27f26f4c0cd12edec7daed0
OLIWIERTE21C <oliwier.kondratowicz@elev.ga.ntig.se>
2024-05-07 14:11:59 +0200

    SKREV ASV

---
+++ src/MulticastChatServer.java
@@ -0,0 +1,31 @@
+import java.net.*;
+
+public class MulticastChatServer {
+    public static void main(String[] args)
+    throws Exception  {
+
+
+        int portnumber = 4444;
+        if(args.length>=1){
+            portnumber=Integer.parseInt(args[0]);
+        }
+
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port "+portnumber);
+
+        InetAddress group = InetAddress.getByName("225.2.5.6");
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
+
+        while (infinite){
+            byte buf[] = new byte[1024];
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+        String msg = new String(data.getData()).trim();
+            System.out.println("Message received from client = "+msg);
+        }
+        serverMulticastSocket.close();
+    }
+
+}
